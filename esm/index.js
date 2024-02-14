c4fabba61f60b2419cffc09d0f45107a8b72d549
webreflection <andrea.giammarchi@gmail.com>
2024-02-14 17:55:28 +0100

    Just a semi

--- esm/index.js
+++ esm/index.js
@@ -4,8 +4,7 @@ const isFunction = value => typeof value === 'function';
 
 const bypass = (p, k, { get, value }) => get || !isFunction(value) ?
                 p.then(r => r[k]) :
-                (...a) => p.then(r => value.apply(r, a))
-;
+                (...a) => p.then(r => value.apply(r, a));
 
 const direct = (p, value) => isFunction(value) ? value.bind(p) : value;
 
